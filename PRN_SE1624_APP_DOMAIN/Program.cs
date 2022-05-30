9b0c99753cc5cef3aed00fe2204fcbaccdf643b3
khoinhmse150853 <khoinhmse150853@fpt.edu.vn>
2022-05-30 13:16:27 +0700

    Khoi 30/05/2022

---
+++ PRN_SE1624_APP_DOMAIN/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Reflection;
+
+namespace PRN_SE1624_APP_DOMAIN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AppDomain defaultAd = AppDomain.CurrentDomain;
+            Assembly[] loadAssemblies = defaultAd.GetAssemblies();
+            Console.WriteLine($"The assemblies loaded in {defaultAd.FriendlyName}");
+            foreach(Assembly a in loadAssemblies)
+            {
+                Console.WriteLine($"Name: {a.GetName().Name}, Version: {a.GetName().Version}");
+            }
+        }
+    }
+}
